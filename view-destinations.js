b3bd8ca03ad4e792b84d2511eb41f1be0ac23344
Mathiassren <110809330+Mathiassren@users.noreply.github.com>
2023-03-06 16:28:03 +0100

    Added js wich favorise destination by fa-heart.

--- view-destinations.js
+++ view-destinations.js
@@ -12,3 +12,40 @@ fetch(`./data/${id}.json`)
             <li>${data.facilities}</li>
         `;
   });
+
+
+ 
+  document.addEventListener("DOMContentLoaded", () => {
+
+    const heartIcons = document.querySelectorAll(".fa-heart");
+  
+   
+    heartIcons.forEach((icon) => {
+
+      const destinationId = icon.closest("figure").getAttribute("id");
+  
+      const isFavorited = localStorage.getItem(destinationId);
+  
+  
+      if (isFavorited) {
+        icon.classList.add("fa-solid");
+      }
+    });
+  });
+  
+  document.querySelector(".destinationslist").addEventListener("click", (e) => {
+    if (e.target.classList.contains("fa-heart")) {
+
+      const destinationId = e.target.closest("figure").getAttribute("id");
+  
+      if (!localStorage.getItem(destinationId)) {
+        localStorage.setItem(destinationId, "favorited");
+        e.target.classList.add("fa-solid");
+      } else {
+        localStorage.removeItem(destinationId);
+        e.target.classList.remove("fa-solid");
+      }
+    }
+  });
+  
+  
